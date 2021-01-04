13653e03f7ff0aeec44ea90caf3e28e9f8be9547
wzy <1304847504@qq.com>
2021-01-04 16:50:08 +0800

    首次提交

---
+++ src/main/java/com/shangma/cn/HuigeDemo2.java
@@ -0,0 +1,11 @@
+package com.shangma.cn;
+
+/**
+ * @author shangmaxueyuan
+ * @create 2021-01-04 16:37
+ */
+public class HuigeDemo2 {
+    public static void main(String[] args) {
+        System.out.println("我要打印了");
+    }
+}
